8673b93fb5c346f610a927feb1622a83e06c3f5e
Manabu Matsuzaki <matsumana@gmail.com>
2013-03-07 05:34:39 +0900

    fluent-agent-liteのクックブック修正

--- cookbooks/fluent-agent-lite/recipes/default.rb
+++ cookbooks/fluent-agent-lite/recipes/default.rb
@@ -1,13 +1,20 @@
-script 'install' do
-  interpreter 'bash'
-  flags '-e'
-  code <<-'EOF'
-    git clone git://github.com/tagomoris/fluent-agent-lite.git /usr/local/src
-    cd /usr/local/src
-    fluent-agent-lite/bin/install.sh
-  EOF
+# clone済みだとエラーになるので、ディレクトリがない場合だけ実行する 
+unless File.directory?("/usr/local/src/fluent-agent-lite") then
+  script 'install' do
+    interpreter 'bash'
+    flags '-e'
+    code <<-'EOF'
+      git clone git://github.com/tagomoris/fluent-agent-lite.git /usr/local/src/fluent-agent-lite
+      cd /usr/local/src
+      fluent-agent-lite/bin/install.sh
+    EOF
+  end
 end
 
+# デフォルト設定ファイルのLogFormatの末尾に%Dを追加しただけです。
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+# ↓
+# LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %D" combined
 cookbook_file '/etc/httpd/conf/httpd.conf' do
   source 'etc/httpd/conf/httpd.conf'
   group  'root'
