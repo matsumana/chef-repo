f09f71dda73f2fd80444637dbf10a3aad680025c
Manabu Matsuzaki <matsumana@gmail.com>
2013-03-06 22:06:12 +0900

    td-agentのレシピ追加

---
+++ cookbooks/td-agent/recipes/default.rb
@@ -0,0 +1,44 @@
+cookbook_file '/etc/yum.repos.d/td.repo' do
+  source 'etc/yum.repos.d/td.repo'
+  group  'root'
+  owner  'root'
+  mode   '0644'
+end
+
+script 'install' do
+  interpreter 'bash'
+  flags '-e'
+  code <<-'EOF'
+    yum install -y td-agent
+  EOF
+end
+
+cookbook_file '/etc/td-agent/td-agent.conf' do
+  source 'etc/td-agent/td-agent.conf'
+  group  'root'
+  owner  'root'
+  mode   '0644'
+end
+
+script 'gem_install' do
+  interpreter 'bash'
+  flags '-e'
+  code <<-'EOF'
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-parser
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-rewrite-tag-filter
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-forest
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-numeric-counter
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-datacounter
+    /usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-growthforecast
+  EOF
+end
+
+script 'start_daemon' do
+  interpreter 'bash'
+  flags '-e'
+  code <<-'EOF'
+    chkconfig td-agent on
+    chkconfig td-agent --list
+    service td-agent start
+  EOF
+end
